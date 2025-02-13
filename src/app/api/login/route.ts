6ded112c9dcc55613ecc80e3a64677aaefb60b4c
Rafiq Shaik <rafiqshaik097@gmail.com>
2025-02-13 10:59:27 +0530

    fix: improve error handling in login API; ensure proper response for user not found and incorrect password

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,22 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextRequest, NextResponse } from "next/server";
 import { User } from "@/lib/models/User";
 
-export async function post(request: NextRequest) {
-  const { email, password } = await request.json();
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
 
-  const user = await User.findOne({ email: email });
-  if (!user) {
-    return NextResponse.json({ message: "User not found" }, { status: 404 });
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    const correct = await user.comparePassword(password);
+    if (!correct) {
+      return NextResponse.json(
+        { message: "Incorrect Password" },
+        { status: 401 }
+      );
+    }
+
+    return NextResponse.json({ message: "Login Successful" }, { status: 200 });
+  } catch (err: any) {
+    console.log(err?.message);
+    return NextResponse.json({ message: err?.message }, { status: 500 });
   }
-
-  const correct = await user.comparePassword(password);
-  if (!correct) {
-    return NextResponse.json(
-      { message: "Incorrect Password" },
-      { status: 401 }
-    );
-  }
-
-  return NextResponse.json({ message: "Login Successful" }, { status: 200 });
-
 }
