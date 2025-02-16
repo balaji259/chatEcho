0f3438039050dd1d301b8927d71cef95055c5846
Rafiq Shaik <rafiqshaik097@gmail.com>
2025-02-16 15:13:53 +0530

    feat: implement JWT authentication and improve login response handling

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import bcryptjs from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
 import { User } from "@/lib/models/User";
 import connectDB from "@/lib/db";
+// import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export async function POST(request: NextRequest) {
   try {
@@ -23,15 +24,20 @@ export async function POST(request: NextRequest) {
       );
     }
 
-    // const correct = await bcryptjs.compare(password, user.password);
-    // if (!correct) {
-    //   return NextResponse.json(
-    //     { message: "Incorrect Password" },
-    //     { status: 401 }
-    //   );
-    // }
+    const correct = await password == user.password;
+    if (!correct) {
+      return NextResponse.json(
+        { message: "Incorrect Password" },
+        { status: 401 }
+      );
+    }
 
-    return NextResponse.json({ message: "Login Successful" }, { status: 200 });
+    const token = jwt.sign(user, process.env.SECRET_KEY!, { expiresIn: "30d" });
+
+    const response = NextResponse.json({ message: "Login Successful", user }, { status: 200 });
+    response.cookies.set("token", token);
+
+    return response;
   } catch (err: any) {
     console.log(err?.message);
     return NextResponse.json({ message: err?.message }, { status: 500 });
